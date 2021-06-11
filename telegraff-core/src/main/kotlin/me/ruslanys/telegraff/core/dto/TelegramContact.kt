10c77b69999c10ad9254375f60c4650320f9efe2
Ruslan Gazanfarov <ruslan.gazanfarov@wearebrain.com>
2021-06-11 12:11:16 +0300

    getters added for telegram contact parsing

--- telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dto/TelegramContact.kt
+++ telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dto/TelegramContact.kt
@@ -4,19 +4,17 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 
 data class TelegramContact(
-
-    @JsonProperty("user_id")
+    @get:JsonProperty("user_id")
     val userId: Long,
 
-    @JsonProperty("first_name")
+    @get:JsonProperty("first_name")
     val firstName: String,
 
-    @JsonProperty("last_name")
+    @get:JsonProperty("last_name")
     val lastName: String?,
 
-    @JsonProperty("phone_number")
+    @get:JsonProperty("phone_number")
     val phoneNumber: String?
-
 ) {
     // This will be returned to validation block which will be responsible for parsing it back as a contact entity
     fun getContact(): String = ObjectMapper().writeValueAsString(this)
