548c521fc2c766065ca63b173f60103a7fa09495
Ruslan Gazanfarov <ruslan.gazanfarov@wearebrain.com>
2021-06-17 10:08:46 +0300

    callback query answer problem fixed logs refactored

--- telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/filter/CallbackQueryAnswerFilter.kt
+++ telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/filter/CallbackQueryAnswerFilter.kt
@@ -4,6 +4,7 @@ import me.ruslanys.telegraff.core.annotation.TelegramFilterOrder
 import me.ruslanys.telegraff.core.component.TelegramApi
 import me.ruslanys.telegraff.core.dto.TelegramMessage
 import me.ruslanys.telegraff.core.filter.FilterOrders.Companion.CALLBACK_QUERY_ANSWER_FILTER_ORDER
+import org.slf4j.LoggerFactory
 
 @TelegramFilterOrder(CALLBACK_QUERY_ANSWER_FILTER_ORDER)
 class CallbackQueryAnswerFilter(
@@ -13,10 +14,18 @@ class CallbackQueryAnswerFilter(
     override fun handleMessage(message: TelegramMessage, chain: TelegramFilterChain) {
         if (message.callbackQuery != null) {
             // Sending callback query answer to telegram api
-            telegramApi.sendAnswerCallbackQuery(message.callbackQuery?.id ?: 0L)
+            try {
+                telegramApi.sendAnswerCallbackQuery(message.callbackQuery?.id ?: 0L)
+            } catch (e: Throwable) {
+                log.error("Error sending callback query answer: ${e.printStackTrace()}")
+            }
             chain.doFilter(message)
         } else {
             chain.doFilter(message)
         }
+    }
+
+    private companion object {
+        private val log = LoggerFactory.getLogger(CallbackQueryAnswerFilter::class.java)
     }
 }
