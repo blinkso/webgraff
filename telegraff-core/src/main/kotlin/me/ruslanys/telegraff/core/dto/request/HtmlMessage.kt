da02fef80ebe367a68b9e956d52287cc82af7616
Ruslan Gazanfarov <ohthatrun@gmail.com>
2022-02-14 13:18:56 +0200

    registration form creation localizations updated

--- telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dto/request/HtmlMessage.kt
+++ telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dto/request/HtmlMessage.kt
@@ -2,14 +2,12 @@ package me.ruslanys.telegraff.core.dto.request
 
 import me.ruslanys.telegraff.core.dto.request.keyboard.TelegramMarkupReplyKeyboard
 import me.ruslanys.telegraff.core.dto.request.keyboard.TelegramRemoveReplyKeyboard
-import java.util.*
 
-class HtmlMessage(text: String, vararg replies: String, locale: Locale?, chatId: Long = 0) : TelegramMessageSendRequest(
+class HtmlMessage(text: String, vararg replies: String, chatId: Long = 0) : TelegramMessageSendRequest(
     chatId,
     text,
     TelegramParseMode.HTML,
     if (replies.isNotEmpty()) TelegramMarkupReplyKeyboard(
-        answers = replies.asList(),
-        locale = locale
+        answers = replies.asList()
     ) else TelegramRemoveReplyKeyboard()
 )
--- telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dto/request/MarkdownMessage.kt
+++ telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dto/request/MarkdownMessage.kt
@@ -2,15 +2,19 @@ package me.ruslanys.telegraff.core.dto.request
 
 import me.ruslanys.telegraff.core.dto.request.keyboard.TelegramMarkupReplyKeyboard
 import me.ruslanys.telegraff.core.dto.request.keyboard.TelegramRemoveReplyKeyboard
-import java.util.*
 
-class MarkdownMessage(text: String, vararg replies: String, locale: Locale?, chatId: Long = 0) :
+class MarkdownMessage(
+    text: String,
+    vararg replies: String,
+    cancelButtonText: String? = null,
+    chatId: Long = 0
+) :
     TelegramMessageSendRequest(
         chatId,
         text,
         TelegramParseMode.MARKDOWN,
         if (replies.isNotEmpty()) TelegramMarkupReplyKeyboard(
             answers = replies.asList(),
-            locale = locale
+            cancelButtonText = cancelButtonText
         ) else TelegramRemoveReplyKeyboard()
     )
